31d56f87815321988ae3cdbc9c0c54174a61b7d4
Joram Wambugu <artisan254@gmail.com>
2021-10-11 08:57:43 +0300

    Reformat code after set up

--- quiet_hn/main.go
+++ quiet_hn/main.go
@@ -45,43 +45,51 @@ func parseHNItem(hnItem hn.Item) item {
 }
 
 func handler(numStories int, tpl *template.Template) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
+
 		var client hn.Client
+
 		ids, err := client.TopItems()
 		if err != nil {
 			http.Error(w, "Failed to load top stories", http.StatusInternalServerError)
 			return
 		}
+
 		var stories []item
+
 		for _, id := range ids {
 			hnItem, err := client.GetItem(id)
 			if err != nil {
 				continue
 			}
+
 			item := parseHNItem(hnItem)
 			if isStoryLink(item) {
 				stories = append(stories, item)
+
 				if len(stories) >= numStories {
 					break
 				}
 			}
 		}
+
 		data := templateData{
 			Stories: stories,
 			Time:    time.Now().Sub(start),
 		}
-		err = tpl.Execute(w, data)
-		if err != nil {
+
+		if err := tpl.Execute(w, data); err != nil {
 			http.Error(w, "Failed to process the template", http.StatusInternalServerError)
 			return
 		}
-	})
+	}
 }
 
 func main() {
 	// parse flags
 	var port, numStories int
+
 	flag.IntVar(&port, "port", 3000, "the port to start the web server on")
 	flag.IntVar(&numStories, "num_stories", 30, "the number of top stories to display")
 	flag.Parse()
